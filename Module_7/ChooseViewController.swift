a9052aac66659c57978b92a6e5a41d248424d47a
liubowolkova <10179875+liubowolkova@users.noreply.github.com>
2020-12-18 04:52:00 +0300

    Task 2: elements are added into controllers on the storyboard; needed classes are made; outlets and functions are prepared.

---
+++ Module_7/ChooseViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ChooseViewController.swift
+//  Module_7
+//
+//  Created by Любовь Волкова on 18.12.2020.
+//
+
+import UIKit
+
+class ChooseViewController: UIViewController {
+    @IBOutlet weak var infoTextField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    @IBAction func chooseAndReturn(_ sender: UIButton) {
+    }
+}
---
+++ Module_7/Task2ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  Task2ViewController.swift
+//  Module_7
+//
+//  Created by Любовь Волкова on 18.12.2020.
+//
+
+import UIKit
+
+class Task2ViewController: UIViewController {
+    
+    @IBOutlet weak var infoLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func showNextCntroller(_ sender: UIButton) {
+    }
+}
