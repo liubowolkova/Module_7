657292ad63a27083f71540b6759338de37ef30d4
liubowolkova <10179875+liubowolkova@users.noreply.github.com>
2020-12-19 04:47:35 +0300

    Task 2. Data transfer doesn't work.

--- Module_7/ChooseViewController.swift
+++ Module_7/ChooseViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ChooseViewController: UIViewController {
     @IBOutlet weak var infoTextField: UITextField!
-    
+    var text = "green"
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("did load")
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        print("willAppear")
+        infoTextField.placeholder = "Выбран \(text)"
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        print("new: \(text)")
+    }
+    
     @IBAction func chooseAndReturn(_ sender: UIButton) {
+        
     }
 }
--- Module_7/Task2ViewController.swift
+++ Module_7/Task2ViewController.swift
@@ -10,11 +10,20 @@ import UIKit
 class Task2ViewController: UIViewController {
     
     @IBOutlet weak var infoLabel: UILabel!
+    var textColor = "blue"
+    var backgroundColor = UIColor.green
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.infoLabel.text = "Выбран \(textColor)"
     }
     
     @IBAction func showNextCntroller(_ sender: UIButton) {
+        if let vc = storyboard?.instantiateViewController(identifier: "ChooseController") as? ChooseViewController {
+            print("this \(self.textColor)")
+        vc.text = self.textColor
+        print("in func")
+        show(vc, sender: nil)
+        }
     }
 }
